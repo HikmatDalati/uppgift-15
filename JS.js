f1fe2d91c2c9d493f6c0762142157713b078f845
HikmatDalati <58488387+HikmatDalati@users.noreply.github.com>
2020-01-11 21:14:04 +0100

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,18 @@
+/*
+var color=0;
+function changeBg(){
+	var bg = document.getElementById('bg');
+	if (color==0) {
+		bg.classList.add('whiteBg');
+		color=1;
+	}
+	else{
+		bg.classList.add('blackBg');
+		color=0;
+	}
+}
+*/
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
